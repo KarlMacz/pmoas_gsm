910afb92e6c80d82ee9d52758ea324bacacce122
Karl T. Macadangdang <karljarren0308@gmail.com>
2018-03-17 18:06:44 +0800

    Initial commit

---
+++ app.js
@@ -0,0 +1,32 @@
+var electron = require('electron');
+
+var app = electron.app;
+var BrowserWindow = electron.BrowserWindow;
+var Menu = electron.Menu;
+
+app.on('window-all-closed', function() {
+    if(process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('ready', function() {
+    var mainWindow = new BrowserWindow({
+        show: false,
+        backgroundColor: '#eee',
+        height: 600,
+        width: 800,
+        resizable: false,
+        frame: false,
+        autoHideMenuBar: true,
+        fullscreen: false
+    });
+
+    mainWindow.loadURL('file://' + __dirname + '/system/index.html');
+    // mainWindow.openDevTools();
+    Menu.setApplicationMenu(null);
+
+    mainWindow.on('ready-to-show', function() {
+        mainWindow.show();
+    });
+});
