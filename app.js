4b09b9ac9629f4d28dd84bce1e94d52535c282a7
Karl T. Macadangdang <karljarren0308@gmail.com>
2018-03-20 22:52:04 +0800

    Uncommented some lines

--- app.js
+++ app.js
@@ -94,17 +94,17 @@ app.on('ready', function() {
 
         socket.on('gsm_command', function(data) {
             data = JSON.parse(data);
-            // gsmModule.write('AT\r\n', function() {
-                // gsmModule.write('AT+CREG=1\r\n', function() {
-                    // gsmModule.write('AT+CMGF=1\r\n', function() {
-                        // gsmModule.write('AT+CMGS="' + data.contact_number + '"\r\n', function() {
-                            // gsmModule.write(data.message + String.fromCharCode(26) + '\r\n', function() {
+            gsmModule.write('AT\r\n', function() {
+                gsmModule.write('AT+CREG=1\r\n', function() {
+                    gsmModule.write('AT+CMGF=1\r\n', function() {
+                        gsmModule.write('AT+CMGS="' + data.contact_number + '"\r\n', function() {
+                            gsmModule.write(data.message + String.fromCharCode(26) + '\r\n', function() {
                                 io.emit('gsm_sms_sent', true);
-                            // });
-                        // });
-                    // });
-                // });
-            // });
+                            });
+                        });
+                    });
+                });
+            });
         });
 
         socket.on('gsm_disconnect', function(data) {
